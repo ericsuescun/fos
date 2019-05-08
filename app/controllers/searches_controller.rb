6c466509f6b0665007b2a1b7517d9792474bb3d9
Eric Suescun <edsuescun@gmail.com>
2019-05-08 15:17:36 -0500

    Se implementa form_tag con busqueda y controlador para la misma

---
+++ app/controllers/searches_controller.rb
@@ -0,0 +1,11 @@
+class SearchesController < ApplicationController
+
+	def index
+		@questions = Question.where("title like ?", "%#{params[:search]}%")
+	end
+
+	private
+		def searches_params
+			params.require(:searches).permit(:search)
+		end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -16,4 +16,6 @@ Rails.application.routes.draw do
 
 	resources :users, only: [:new, :create]
 
+	resources :searches, only: [:index]
+
 end
